77560466ff3e9bdcba273f9c1c7138c17d4700a2
gyan <gyanender.kumar@algoworks.com>
2021-05-01 16:44:29 +0530

    Db updated to postgres

--- Dockerfile
+++ Dockerfile
@@ -6,5 +6,3 @@ COPY requirement.text /usr/src/app
 EXPOSE 8000
 RUN pip install -r requirement.text
 COPY ./ /usr/src/app
-RUN python manage.py migrate
-RUN python manage.py loaddata --app city_info fixtures.json
--- SMSGroup/settings.py
+++ SMSGroup/settings.py
@@ -77,8 +77,12 @@ CORS_ORIGIN_ALLOW_ALL = True
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'sms_group',
+        'USER': 'gyan',
+        'PASSWORD': 'gyan123',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
